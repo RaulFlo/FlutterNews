56b92d8b5c3b3068793601f98a619189c5972ece
Raul Flores <raulflo2012@gmail.com>
2021-11-17 16:49:39 -0600

    Tweak botnavbar: cliprect, safearea

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Future<NewsModel> _newsModel;
-  int index;
+  int index = 0;
 
   @override
   void initState() {
@@ -29,76 +29,85 @@ class _HomePageState extends State<HomePage> {
       Icon(Icons.settings, size: 30),
     ];
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('News App'),
-        centerTitle: true,
-        toolbarHeight: 50,
-      ),
-      body: Container(
-        child: FutureBuilder<NewsModel>(
-          future: _newsModel,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data.articles.length,
-                  itemBuilder: (context, index) {
-                    var article = snapshot.data.articles[index];
-                    var formattedTime = DateFormat('dd MMM - HH:mm')
-                        .format(article.publishedAt);
-                    return Container(
-                      height: 100,
-                      margin: const EdgeInsets.all(8),
-                      child: Row(
-                        children: <Widget>[
-                          Card(
-                            clipBehavior: Clip.antiAlias,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24),
-                            ),
-                            child: AspectRatio(
-                                aspectRatio: 1,
-                                child: Image.network(
-                                  article.urlToImage,
-                                  fit: BoxFit.cover,
-                                )),
-                          ),
-                          SizedBox(width: 16),
-                          Flexible(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        top: false,
+        child: ClipRect(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('News App'),
+              centerTitle: true,
+              toolbarHeight: 50,
+            ),
+            body: Container(
+              child: FutureBuilder<NewsModel>(
+                future: _newsModel,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data.articles.length,
+                        itemBuilder: (context, index) {
+                          var article = snapshot.data.articles[index];
+                          var formattedTime = DateFormat('dd MMM - HH:mm')
+                              .format(article.publishedAt);
+                          return Container(
+                            height: 100,
+                            margin: const EdgeInsets.all(8),
+                            child: Row(
                               children: <Widget>[
-                                Text(formattedTime),
-                                Text(
-                                  article.title,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold),
+                                Card(
+                                  clipBehavior: Clip.antiAlias,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(24),
+                                  ),
+                                  child: AspectRatio(
+                                      aspectRatio: 1,
+                                      child: Image.network(
+                                        article.urlToImage,
+                                        fit: BoxFit.cover,
+                                      )),
                                 ),
-                                Text(
-                                  article.description,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
+                                SizedBox(width: 16),
+                                Flexible(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(formattedTime),
+                                      Text(
+                                        article.title,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        article.description,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ],
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-            } else
-              return Center(child: CircularProgressIndicator());
-          },
+                          );
+                        });
+                  } else
+                    return Center(child: CircularProgressIndicator());
+                },
+              ),
+            ),
+            bottomNavigationBar: CurvedNavigationBar(
+              height: 50,
+              items: items,
+              buttonBackgroundColor: Colors.blue.shade100,
+              onTap: (index) => setState(
+                  () => this.index = index), //making this state the index
+            ),
+          ),
         ),
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 60,
-        items: items,
-        buttonBackgroundColor: Colors.blue.shade100,
-        onTap: (index) =>
-            setState(() => this.index = index), //making this state the index
       ),
     );
   }
