023c68dfd976f9e01c758ffeb15839c565e4361f
Raul Flores <raulflo2012@gmail.com>
2021-11-16 19:06:46 -0600

    Add newsinfo Model cont: Api_manager

---
+++ lib/constants/strings.dart
@@ -0,0 +1,5 @@
+class Strings{
+  static String news_url = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=d5fbc5b8ad644ca3953c7dfb94823033';
+
+
+}
--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,7 +1,29 @@
+import 'dart:convert';
+import 'dart:html';
+
 import 'package:http/http.dart' as http;
+import 'package:news_demo/constants/strings.dart';
+import 'package:news_demo/models/newsinfo.dart';
 
 class API_Manager {
-  void getNews() {
+  Future<NewsModel> getNews() async {
     var client = http.Client();
+    var url = Uri.parse(Strings.news_url);
+
+    var newsModel = null;
+
+    try {
+      var response = await client.get(url);
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        var jsonMap = jsonDecode(jsonString);
+
+        var newsModel = NewsModel.fromJson(jsonMap);
+      }
+    } catch (Exception) {
+      return newsModel;
+    }
+
+    return newsModel;
   }
 }
