7ef9976bd65b053f9e155b3e03ea739177a61b47
Raul Flores <raulflo2012@gmail.com>
2021-11-16 18:11:41 -0600

    Add Api_Manager

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -13,18 +13,15 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('News App'),
-        ),
-        
+      ),
       body: Container(
         child: ListView.builder(itemBuilder: (context, index) {
-            return Container(
-              height: 100,
-              color: Colors.red,
-            );
-
+          return Container(
+            height: 100,
+            color: Colors.teal,
+          );
         }),
       ),
-      
     );
   }
 }
---
+++ lib/services/api_manager.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+
+class API_Manager {
+  void getNews() {
+    var client = http.Client();
+  }
+}
